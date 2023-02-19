3b87b550d4540c3e521310f7bdf32e4a3eab8b43
Matthieudegny <matthieu.dev.degny@gmail.com>
2023-02-19 19:00:28 +0100

    update adress back

--- src/page/adressBack.js
+++ src/page/adressBack.js
@@ -1,1 +1,3 @@
-export const adressBack = "http://localhost/8080";
+// export const adressBack = "http://localhost/8080";
+export const adressBack =
+  "https://mern-ia-app-back-git-main-matthieudegny.vercel.app";
